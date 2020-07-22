c3c6395c51fb8df19e9e022d910ddcdcdaf3c4b3
Royce Williams <roycejames1130@gmail.com>
2020-07-22 10:23:22 -0700

    SQL all set up and the starter code for the logic is there

---
+++ employees.sql
@@ -0,0 +1,83 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+USE company_db;
+
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+USE company_db;
+
+CREATE TABLE employees(
+     id INT NOT NULL AUTO_INCREMENT,
+     first_Name VARCHAR(30) NOT NULL,
+     last_Name  VARCHAR(30) NOT NULL,
+     position VARCHAR(30) NOT NULL,
+     manager_id INT,
+     PRIMARY KEY (id)
+);
+
+INSERT INTO employees (first_Name, last_Name, position,manager_id)
+VALUES ("Woyce", "Rilliams", "CEO", NULL);
+
+
+INSERT INTO employees (first_Name, last_Name, position, manager_id)
+VALUES ("David", "Reback", "Engineer", 1);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Engineer", 70000.00, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Scientist", 80000.00, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Secretary", 38000.00, 2);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Support Line", 40000.00, 2);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Stock Specialist", 65000.00, 3);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Crowd Funder", 55000.00, 3);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("CEO", 999999.99, 4);
+
+
+INSERT INTO department (name)
+VALUES ("Research");
+
+
+INSERT INTO department (name)
+VALUES ("Customer Service");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+
+INSERT INTO department (name)
+VALUES ("Management");
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+var cTable = require('console.table');
+var logic = require("./index.js")
 
 var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 8080,
+  port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
   password: "password",
-  database: "employeesDB"
+  database: "company_db"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  afterConnection()
 });
+
+function afterConnection(){
+    connection.query("SELECT * FROM employees", function(err, res) {
+        if (err) throw err;
+        console.log("Welcome to Employee Tracker, the terminal ran application for managing your business!");
+        console.log("For starters, here's a table of all of your current employees.");    
+        console.table(res);
+        logic();
+      });
+}
+
+function insertEmployee(data){
+
+}
+
+module.exports = connection 
